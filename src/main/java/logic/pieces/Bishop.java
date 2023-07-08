7f8501460c44d29a9fdff8d8a5df6811e3f17c35
David Ruppert <david.ruppert@student.fhws.de>
2023-07-08 16:04:23 +0200

    Bishop moves working

--- src/main/java/logic/pieces/Bishop.java
+++ src/main/java/logic/pieces/Bishop.java
@@ -5,6 +5,13 @@ import logic.Move;
 import util.GameColor;
 import util.PieceType;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static util.Constants.boardSize;
+import static util.Functions.tryGetField;
+
 public class Bishop extends Piece {
 
     public Bishop(GameColor pieceColor, Field[][] fields, Field field) {
@@ -13,8 +20,50 @@ public class Bishop extends Piece {
 
     @Override
     public Move[] getPossibleMoves() {
-        // TODO
-        return new Move[0];
+        int x = field.getX();
+        int y = field.getY();
+
+        List<Optional<Field>> toFields = new ArrayList<>();
+        boolean plusPlusDirectionBlocked = false;
+        boolean plusMinusDirectionBlocked = false;
+        boolean minusPlusDirectionBlocked = false;
+        boolean minusMinusDirectionBlocked = false;
+        for(int delta = 1; delta < boardSize; delta++) {
+            if(!plusPlusDirectionBlocked) {
+                Optional<Field> plusPlusField = tryGetField(x + delta, y + delta, fields);
+                plusPlusDirectionBlocked = plusPlusField.flatMap(Field::getPiece).isPresent();
+                toFields.add(plusPlusField);
+            }
+
+            if(!plusMinusDirectionBlocked) {
+                Optional<Field> plusMinusField = tryGetField(x + delta, y - delta, fields);
+                plusMinusDirectionBlocked = plusMinusField.flatMap(Field::getPiece).isPresent();
+                toFields.add(plusMinusField);
+            }
+
+            if(!minusPlusDirectionBlocked) {
+                Optional<Field> minusPlusField = tryGetField(x - delta, y + delta, fields);
+                minusPlusDirectionBlocked = minusPlusField.flatMap(Field::getPiece).isPresent();
+                toFields.add(minusPlusField);
+            }
+
+            if(!minusMinusDirectionBlocked) {
+                Optional<Field> minusMinusField = tryGetField(x - delta, y - delta, fields);
+                minusMinusDirectionBlocked = minusMinusField.flatMap(Field::getPiece).isPresent();
+                toFields.add(minusMinusField);
+            }
+        }
+
+        return toFields
+                .stream()
+                .flatMap(Optional::stream)
+                .filter(a -> a
+                        .getPiece()
+                        .map(Piece::getColor)
+                        .map(color -> color != this.getColor())
+                        .orElse(true))
+                .map(a -> new Move(field, a))
+                .toArray(Move[]::new);
     }
 
     @Override
